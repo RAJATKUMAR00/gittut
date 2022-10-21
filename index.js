ac8d522c3fcccf417661127566eb8e832bfdfcf2
RAJAT BAUDH <rajatporiya1996@gmail.com>
2022-10-21 06:27:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ for(var i=0; i<items.length; i++){
     items[i].style.fontWeight = 'bold';
     items[i].style.backgroundColor = '#f4f4f4';
 }
+
+// And try editing it with getelementsbyclassname and 
+// then by getelementbytagname
+
+getelementbytagname
+var newl = document.getElementsByTagName('li');
+
+newl.style.display = 'none';   // show error because of bootstrap form 
+
+// getelementsbyclassname
+var newT = document.getElementsByClassName('lit');
+
+newT.style.backgroundColor = 'yellow';   // show error because of bootstrap form 
